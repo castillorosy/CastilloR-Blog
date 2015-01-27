49f5edb113efc562092756b750077b6dc3443318
castillorosy <rosycastillo18@yahoo.com>
2015-01-27 13:46:44 -0800

    1st commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo"Web Server is Running Locally";
+        ?>
+    </body>
+</html>
