661f1c344b68582031fa6ba038e14a9a8c5f51ca
castillorosy <rosycastillo18@yahoo.com>
2015-01-30 14:02:47 -0800

    idk

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,12 +1,12 @@
 <!--make sure that this info. goes to the database file-->
 <!--going to include the info we have here to the php file-->
 <?php
-require_once(__DIR__ . "../model/databasse.php");
+require_once (__DIR__ . "/../model/database.php");
 
 $connection = new mysqli ($host, $username, $password);
 //showing why the program shut off
 if($connection->connect_error){
-    die("Error: " . $connection->connect_error);
+   die("<p>Error: " . $connection->connect_error . "</p>");
 }
 //"!" means it doesnt exists
 $exists = $connection->select_db($database);
@@ -14,11 +14,26 @@ if(!$exists) {
    $query = $connection->query("CREATE DATABASE $database");
    
    if($query){
-       echo "Successfully created database" . $database;
+       echo "<p>Successfully created database: " . $database . "</p>";
    }
 }
  else {
-    echo "Database alreadyc exists.";
+    echo "<p>Database already exists.</p>";
+}
+//creating tables by connection tables together that we are
+//able to put into text
+$query = $connection->query("CREATE TABLE  posts ("
+        . "id int (11) NOT NULL AUTO_INCREMENT,"
+        ."title  varchar (255) NOT NULL,"
+        . "post text NOT NULL,"
+        . "PRIMARY KEY (id))");
+
+if($query){
+    echo "<p>Successfully created table: post</p>"; 
+}
+
+else {
+    echo "<p>$connection->error</p>";
 }
 //^ helps us make a query
 $connection->close();
--- index.php
+++ index.php
@@ -1,12 +1,5 @@
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-<!--        echo means its running -->
-        <?php
-            require_once(__DIR__ . "/controller/create-db.php");
-        ?>
-    </body>
-</html>
+
+<?php
+
+require_once (__DIR__ . "/controller/create-db.php");
+?>
