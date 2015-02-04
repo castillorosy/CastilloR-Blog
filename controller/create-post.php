5f9fef6a6ebda76d843a256ec05324558818f13d
castillorosy <rosycastillo18@yahoo.com>
2015-02-04 13:58:49 -0800

    aye

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,7 +1,17 @@
 <?php
 
+require_once (__DIR__ . "/../model/database.php");
+
+$connection = new mysqli($host, $username, $password, $database);
+
 $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
 $post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
 
-echo "<p>Title: $title</post>";
-echo "<p>Post: $post</p>";
+$query = $connection->query("INSEERT INTO posts SET title = '$title', post = '$post'");
+
+if(){
+    
+}
+
+$connection->close();
+
