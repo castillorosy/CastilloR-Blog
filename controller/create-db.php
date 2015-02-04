3ca8899f0497f1b62924557187ae10c04941ac80
castillorosy <rosycastillo18@yahoo.com>
2015-02-04 13:45:28 -0800

    video 22

--- controller/create-db.php
+++ controller/create-db.php
@@ -3,36 +3,33 @@
 <?php
 require_once (__DIR__ . "/../model/database.php");
 
-$connection = new mysqli ($host, $username, $password);
+$connection = new mysqli($host, $username, $password);
 //showing why the program shut off
-if($connection->connect_error){
-   die("<p>Error: " . $connection->connect_error . "</p>");
+if ($connection->connect_error) {
+    die("<p>Error: " . $connection->connect_error . "</p>");
 }
 //"!" means it doesnt exists
 $exists = $connection->select_db($database);
-if(!$exists) {
-   $query = $connection->query("CREATE DATABASE $database");
-   
-   if($query){
-       echo "<p>Successfully created database: " . $database . "</p>";
-   }
-}
- else {
+if (!$exists) {
+    $query = $connection->query("CREATE DATABASE $database");
+
+    if ($query) {
+        echo "<p>Successfully created database: " . $database . "</p>";
+    }
+} else {
     echo "<p>Database already exists.</p>";
 }
 //creating tables by connection tables together that we are
 //able to put into text
 $query = $connection->query("CREATE TABLE  posts ("
         . "id int (11) NOT NULL AUTO_INCREMENT,"
-        ."title  varchar (255) NOT NULL,"
+        . "title  varchar (255) NOT NULL,"
         . "post text NOT NULL,"
         . "PRIMARY KEY (id))");
 
-if($query){
-    echo "<p>Successfully created table: post</p>"; 
-}
-
-else {
+if ($query) {
+    echo "<p>Successfully created table: post</p>";
+} else {
     echo "<p>$connection->error</p>";
 }
 //^ helps us make a query
--- view/form.php
+++ view/form.php
@@ -1,8 +1,19 @@
+<?php
+require_once (__DIR__ . "/../model/config.php");
+?>
 <h1>Create Blog Post</h1>
-<form>
+<!--basically saying to send information-->
+<form method="post" action="<?php echo $path . "controller/create-post.php"; ?>">
     <div>
     </div>
     <label for="title">Title:</label>
+    <input type="text" name="title"/>
     <div>
+        <label for="post">Post: </label>
+        <textarea name="post"<></textarea>
+    </div>
+    <div>
+        <!--        the button helps us put the post on web page/blog-->
+        <button type="submit">Submit</button>
     </div>
 </form>
